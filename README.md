5196e097fb4b9efeefcf822359ca37f6c5aca820
Caleb Hamilton <cjlhamilton@gmail.com>
2018-07-30 15:07:06 +0100

    Switched XML parser to TinyXML (ROS standard) and added XML loading code - now depends on roslib; updated README structure

--- README.md
+++ README.md
@@ -5,12 +5,12 @@
 
 A ROS node for the manipulation of a KUKA robot arm via RSI 3.
 
-Developed for ROS Kinetic.
+Developed for ROS Kinetic and tested on Ubuntu 16.04.
 
 **Keywords:** ros, kinetic, rsi, kuka
 
 
-## License
+### License
 
 The source code is released under the [GNU General Public License v3.0](kuka_rsi_ros_interface/LICENSE).
 
@@ -19,7 +19,15 @@ Maintainer: Caleb Hamilton (cjlhamilton@gmail.com)
 Status: Early development**
 
 
-## With thanks to
+### With thanks to
 
 - Dalton Nuclear Institute (http://www.dalton.manchester.ac.uk/) for funding this project and granting permission to publish as open source;
 - Eren Sezener and Osman Kaya from the Ozyegin University Robotics Laboratory for helping me to understand the RSI protocol through their work on the *KUKA RSI-3 Communicator* project (https://github.com/erensezener/kuka-rsi3-communicator).
+
+
+## Dependencies
+[to do]
+
+
+## Installation
+[to do]
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
 // Standard includes
-// #include <iostream>
-// #include <vector>
-// #include <map>
-// #include <set>
-// #include <utility>
-// #include <fstream>
-#include <string>
-// #include <exception>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Socket and network handling
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 // ROS
 #include <ros/ros.h>
+#include <ros/package.h>
 
 // YAML parsing
 // #include "yaml-cpp/yaml.h"
 
 // XML parsing
-#include "pugixml.hpp"
-
-// Exceptions
-// #include "SomeException.h"
+#include <tinyxml.h>
 
 // Messages
 #include "kuka_rsi_ros_interface/KukaPose.h"
@@ -27,23 +25,15 @@
 #include "kuka_rsi_ros_interface/MoveToPose.h"
 #include "kuka_rsi_ros_interface/GetPose.h"
 
-// For socket handling
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
-#include <string.h>
-
 
 // Global variables
 
 static const std::string NODE_NAME = "kuka_rsi_ros_interface";
+static const std::string PACKAGE_PATH = ros::package::getPath(NODE_NAME);
 
 static const char* SERVER_ADDRESS = "172.31.1.146";
 static const uint16_t SERVER_PORT = 49152;
 static const unsigned int BUFFER_SIZE = 1024;
-// sudo ifconfig enp3s0 172.31.1.146 netmask 255.255.255.0 up
 
 
 /*
@@ -86,6 +76,18 @@ int main(int argc, char **argv) {
     ros::ServiceServer move_to_position_service =
         nh.advertiseService("move_to_position", movetoPositionCallback);
     ROS_INFO("Service \"/move_to_position\" started.");
+
+    // Load RSI XML template file
+    std::string xml_file_path = PACKAGE_PATH + "/xml/DataTemplate.xml";
+    TiXmlDocument doc(xml_file_path.c_str());
+    bool is_xml_loaded = doc.LoadFile();
+
+    if (is_xml_loaded) {
+        printf("Loaded RSI XML template file");
+    } else  {
+        ROS_ERROR("Failed to load RSI XML template file.");
+        exit(EXIT_FAILURE);
+    }
 
     // Set up socket
     int server_fd, new_socket, valread;
@@ -137,9 +139,9 @@ int main(int argc, char **argv) {
             printf("received message: \"%s\"\n", buffer);
         }
 
-        buffer.
-
-        sendto(server_fd, buffer, strlen(buffer), 0, (struct sockaddr *) &address, addrlen);
+        // TODO...
+        // buffer.
+        // sendto(server_fd, buffer, strlen(buffer), 0, (struct sockaddr *) &address, addrlen);
     }
 
     ROS_INFO("Closing socket.");
