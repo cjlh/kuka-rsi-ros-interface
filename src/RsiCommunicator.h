93c106c5a1fdddd45742e6e092945443d9072980
Caleb Hamilton <cjlhamilton@gmail.com>
2018-07-31 16:44:49 +0100

    Added initial documentation for data members

--- src/RsiCommunicator.h
+++ src/RsiCommunicator.h
@@ -16,21 +16,33 @@ class RsiCommunicator
 {
     private:
         /*
-         * TODO.
+         * The IP address to which the UDP socket will be bound to.
          */
         std::string ip_address;
 
         /*
-         * TODO.
+         * The port to which the UDP socket will be bound to.
          */
         uint16_t port;
 
+        /*
+         * The size, in bytes, that will be used for buffer arrays.
+         */
         int buffer_size;
 
+        /*
+         * The file descriptor used for the UDP socket.
+         */
         int server_fd;
 
+        /*
+         * Structure to store address information for the UDP socket.
+         */
         struct sockaddr_in address;
 
+        /*
+         * Stores length of the address structure for easy reference.
+         */
         socklen_t addrlen;
 
     public:
