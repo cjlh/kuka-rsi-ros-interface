3d924396cd0aca0e26944fb93c709619e53ce0e4
Caleb Hamilton <cjlhamilton@gmail.com>
2018-08-17 14:42:02 +0100

    Improved link embedding in README

--- README.md
+++ README.md
@@ -21,8 +21,8 @@ Status: In development**
 
 ### Thanks to:
 
-- **The University of Manchester's** Dalton Nuclear Institute (http://www.dalton.manchester.ac.uk/) for funding this project and granting permission to publish as open source;
-- Eren Sezener and Osman Kaya from the Ozyegin University Robotics Laboratory for helping me to understand the RSI protocol through their work on their *KUKA RSI-3 Communicator* project (https://github.com/erensezener/kuka-rsi3-communicator).
+- **The University of Manchester's** [Dalton Nuclear Institute](http://www.dalton.manchester.ac.uk/) for funding this project and granting permission to publish as open source;
+- Eren Sezener and Osman Kaya from the Ozyegin University Robotics Laboratory for helping me to understand the RSI protocol through their work on their [KUKA RSI-3 Communicator](https://github.com/erensezener/kuka-rsi3-communicator) project.
 
 
 ## Prerequisites
--- catkin_ws/src/kuka_rsi_ros_interface/kuka_rsi_ros_interface_core/src/main.cpp
+++ catkin_ws/src/kuka_rsi_ros_interface/kuka_rsi_ros_interface_core/src/main.cpp
@@ -33,51 +33,64 @@ namespace settings
  * TODO.
  */
 int main(int argc, char **argv) {
-    // Initialise ROS node
+    // Initialise ROS node.
     ros::init(argc, argv, settings::node_name);
     ros::NodeHandle nh;
 
-    // Load RSI XML template file
+    // Load RSI XML template file using TinyXML.
     std::string xml_file_path = settings::package_path +
                                 "/xml/DataTemplate.xml";
     TiXmlDocument rsiXmlTemplate(xml_file_path.c_str());
     bool is_xml_loaded = rsiXmlTemplate.LoadFile();
 
+    // Ensure XML file has been read correctly.
     if (is_xml_loaded) {
         ROS_INFO("Loaded RSI XML template file.");
-    } else  {
+    } else {
         ROS_ERROR("Failed to load RSI XML template file. Exiting...");
         exit(EXIT_FAILURE);
     }
 
-    // TODO: check program structure and implement keyboard interrupt
+    // TODO: implement keyboard interrupt
     try {
+        // Create KukaRisRosInterface object, this will be responsible for
+        // the core node logic.
         KukaRsiRosInterface interface(settings::node_name,
                                       settings::server_address,
                                       settings::server_port,
                                       settings::buffer_size,
                                       rsiXmlTemplate);
 
-        // Create service for requesting current position
+        // Create service for requesting current position. The service accepts
+        // an Empty message and returns a message of type KukaPose, containing
+        // the X, Y, Z, A, B, C values of the robot position. For more details,
+        // see the KukaPose class (not the message definition) in this
+        // directory.
         ros::ServiceServer get_position_service =
             nh.advertiseService("get_position_values",
                                 &KukaRsiRosInterface::getPositionValuesCallback,
                                 &interface);
         ROS_INFO("Service \"/get_position_values\" started.");
 
-        // Create service for moving to a position
+        // Create service for moving to a given position. The service accepts a
+        // message of type KukaPose (as described above) and replies with
+        // a boolean "success" message and a string "message" message containing
+        // a success confirmation if the service has executed successfully, or
+        // an error message if not.
         ros::ServiceServer move_to_position_service =
             nh.advertiseService("move_to_position",
                                 &KukaRsiRosInterface::movetoPositionCallback,
                                 &interface);
         ROS_INFO("Service \"/move_to_position\" started.");
 
+        // Begin node execution loop.
         interface.run();
     } catch (const std::exception &e) {
         ROS_FATAL("A non-recoverable error has occurred. Exiting...");
         exit(EXIT_FAILURE);
     }
 
+    // Return 0 for successful execution.
     return 0;
 
 }
